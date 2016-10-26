aedcb0e6570ff5c9d440217879618ddb8b559b12
CsTarepanda <sankaku900622@yahoo.co.jp>
2016-10-26 16:56:28 +0900

    generate tops controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'tops/index'
+
+  get 'tops/result'
+
   root to:'application#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
