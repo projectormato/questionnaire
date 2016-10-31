205684a47abf9d93798e00c8c7e3434da5c00389
projectormato <k.keita811.anuy@gmail.com>
2016-10-31 17:35:19 +0900

    trytrytry

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   get 'result' => 'tops#result'
-
-  root to:'tops#index'
+  root 'tops#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
